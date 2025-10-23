f14765c4e07f6b455bb51166ab5fb3342b0e7b0f
Ana Livia <analiviasantosgarcia2020@gmail.com>
2025-10-23 09:01:59 -0300

    atualização fornecedorescontroller

---
+++ app/Controller/FornecedoresContoller.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "C:/Turma1/xampp/htdocs/Farmacia/Model/FornecedoresModel.php";
+
+class FornecedoresController {
+    private $fornecedoresModel;
+    public function __construct($pdo) {
+        $this->fornecedoresModel = new FornecedoresModel( $pdo);
+    }
+
+    public function listar () {
+        $fornecedores = $this ->fornecedoresModel ->buscarTodos();
+        include_once "C:/Turma1/xampp/htdocs/Farmacia/View/Fornecedores/listar.php";
+        return;
+    }
+
+public function buscarFornecedores($id) {
+    $fornecedores = $this->fornecedoresModel-> buscarFornecedores($id);
+    return $fornecedores;
+ }
+
+
+ public function cadastrar($nome, $tipo, $quantidade, $preco) {
+    return $this->fornecedoresModel-> cadastrar($nome, $tipo, $quantidade, $preco);
+ }
+
+
+ public function editar($nome, $tipo, $quantidade, $preco, $id) {
+    $this->fornecedoresModel-> editar($nome, $tipo, $quantidade,  $preco, $id);
+ }
+
+
+public function deletar($id) {
+    $fornecedores = $this->fornecedoresModel-> deletar($id);
+    return $fornecedores;
+ }
+
+}
+
+?>
