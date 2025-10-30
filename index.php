19d0b5389aca67f5ea0ef30173bc03ac142f6339
kayllayne <kayllaynemaria210@gmail.com>
2025-10-30 08:24:29 -0300

    desin do css

--- index.php
+++ index.php
@@ -7,9 +7,51 @@
 </head>
 <body>
    <link rel="stylesheet" href="css/index.css">
-    <h2>Farma Aura</h2>
-    <h3>“Aura é o brilho invisível que envolve cada um de nós.
-Na FarmaAura, queremos manter esse brilho aceso <br> com cuidado, atenção e amor pela sua saúde.”</h3>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+   <header class="navbar">
+    <div class="logo">
+      <img src="img/Logo FarmaAura.png" alt="Farma Aura">
+      
+    </div>
+    <div class="search-bar">
+      <input type="text" placeholder="Buscar na Farma Aura">
+    </div>
+    <div class="menu-icons">
+      <a href="#">Entrar ou cadastrar</a>
+      <a href="#">Meus pedidos</a>
+      <a href="#">Cesta (R$0,00)</a>
+    </div>
+  </header>
+   
+  <nav class="categorias">
+    <a href="#">Mais Buscados</a>
+    <a href="#">Serviços e Vacinas</a>
+    <a href="#">Manipulação</a>
+    <a href="#">Queridinhos</a>
+    <a href="#">Cupons</a>
+    <a href="#">Perfumes</a>
+    <a href="#">Dose Certa</a>
+  </nav>
+
+  <section class="banner">
+    <div class="banner-texto">
+      <h2>Abasteça sua farmacinha 💊</h2>
+      <p>Com até <strong>25% de desconto</strong> nos seus produtos favoritos.</p>
+      <a href="#" class="botao">Confira</a>
+    </div>
+    <img src="img/remedios.png" alt="Remédios em promoção">
+      <main class="conteudo">
+  
+    <p>Cuide da sua saúde com brilho, amor e bem-estar.</p>
+    <a href="logout.php" class="sair">Sair</a>
+  </main>
+
+  <footer class="rodape">
+    &copy; 2025 Farma Aura — Cuidando da sua energia e bem-estar 🌼
+  </footer>
+
+  
 <h4>Clique abaixo para se cadastrar ou efetuar seu login</h4>
 
 
